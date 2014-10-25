8f1770f8737325ef545fd3e80c2188cba5a48563
Cullen Rhodes <rhodes.cullen@yahoo.co.uk>
2014-10-25 21:55:24 +0100

    Latest views

--- hack2014/project/urls.py
+++ hack2014/project/urls.py
@@ -3,8 +3,11 @@ from django.conf.urls import patterns, url
 from project import views
 
 urlpatterns = patterns('',
-    url(r'$', views.ProjectListView.as_view(), name='project-list'),
-    url(r'create/$', views.ProjectCreateView.as_view(), name='create'),
-    url(r'(?P<user_id>\w+)/(?P<slug>[-_\w]+)/$', views.ProjectView.as_view(), name='detail'),
+    url(r'^$', views.ProjectListView.as_view(), name='project-list'),
+    url(r'categories/$', views.CategoryList.as_view(), name='category-list'),
+    url(r'(?P<slug>[-_\w]+)/$', views.CategoryDetail.as_view(), name='category-detail'),
+    url(r'^create/$', views.ProjectCreateView.as_view(), name='project-create'),
+    url(r'(?P<user_id>\w+)/(?P<slug>[-_\w]+)/add/user/$', views.ProjectAddUserUpdateView.as_view(), name='project-add-user'),
+    url(r'(?P<user_id>\w+)/(?P<slug>[-_\w]+)/$', views.ProjectView.as_view(), name='project-detail'),
     url(r'(?P<user_id>\w+)/$', views.UserProjectListView.as_view(), name='user-project-list'),
 )
--- hack2014/project/views.py
+++ hack2014/project/views.py
@@ -1,12 +1,33 @@
-from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.http import (Http404,
+                         HttpResponse,
+                         HttpResponseRedirect,
+                         HttpResponseBadRequest)
 from django.views.generic import (DetailView,
                                   ListView,
-                                  CreateView)
+                                  CreateView,
+                                  UpdateView)
+from django.core.urlresolvers import reverse_lazy
+from django.template.defaultfilters import slugify
 
 from braces.views import LoginRequiredMixin
 
 
-from project.models import Project
+from project.models import Project, Category
+from project.forms import ProjectCreateForm
+
+
+class CategoryList(ListView):
+    
+    model = Category
+    template_name = 'project/category_list.html'
+    context_object_name = 'categories'
+
+
+class CategoryDetail(DetailView):
+
+    model = Category
+    template_name = 'project/category_detail.html'
 
 
 class ProjectView(DetailView):
@@ -39,7 +60,6 @@ class UserProjectListView(ListView):
     model = Project
     template_name = 'project/user_projects_list.html'
     context_object_name = 'projects'
-
     def get_queryset(self):
         queryset = Project.objects.filter(user__username=self.kwargs.get('user_id'))
         return queryset
@@ -50,4 +70,16 @@ class ProjectCreateView(LoginRequiredMixin, CreateView):
     model = Project
     fields = ['name', 'category', 'description', 'date']
 
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.user = self.request.user
+        self.object.save()
+        
+        return HttpResponseRedirect(reverse_lazy('projects:project-detail',
+            kwargs={'user_id': self.request.user.username, 'slug': self.object.slug}))
 
+
+class ProjectAddUserUpdateView(LoginRequiredMixin, UpdateView):
+    model = Project
+    form_class = ProjectCreateForm
+    template_name_suffix = '_add_user_form'
