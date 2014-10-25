5502bbfd22d783783e5a306a0e73516eecf14f8c
Cullen Rhodes <rhodes.cullen@yahoo.co.uk>
2014-10-25 16:37:06 +0100

    Fixed namespace

--- hack2014/urls.py
+++ hack2014/urls.py
@@ -27,6 +27,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable avatars
     url(r'^avatar/', include('avatar.urls')),
-    url(r'^projects/', include('project.urls'), namespace='projects'),
+    url(r'^projects/', include('project.urls', namespace='projects')),
 
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
